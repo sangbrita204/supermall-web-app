b1fb704ece2d8410f87c158a9b6cfc31a004dd1a
sangbrita saha <sangbritasaha638@gmail.com>
2026-01-02 13:00:21 +0530

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,11 +1,17 @@
+
 import { db } from "./firebase-config.js";
 import { collection, getDocs } from
 "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const list = document.getElementById("offerList");
+const container = document.getElementById("offerList");
 const snap = await getDocs(collection(db, "offers"));
 
 snap.forEach(doc => {
   const o = doc.data();
-  list.innerHTML += `<li>${o.shop} : ${o.details}</li>`;
+  container.innerHTML += `
+    <div class="card">
+      <strong>${o.shop}</strong><br>
+      ${o.details}
+    </div>
+  `;
 });
