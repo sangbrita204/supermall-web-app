ec22b23e6cf104aabaf6dcf461afbeb3a8c9d702
sangbrita saha <sangbritasaha638@gmail.com>
2026-01-02 12:59:29 +0530

    Update shop.js

--- shop.js
+++ shop.js
@@ -2,10 +2,16 @@ import { db } from "./firebase-config.js";
 import { collection, getDocs } from
 "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const list = document.getElementById("shopList");
+const container = document.getElementById("shopList");
 const snap = await getDocs(collection(db, "shops"));
 
 snap.forEach(doc => {
   const s = doc.data();
-  list.innerHTML += `<li>${s.name} | Floor: ${s.floor} | ${s.category}</li>`;
+  container.innerHTML += `
+    <div class="card">
+      <strong>${s.name}</strong><br>
+      Floor: ${s.floor}<br>
+      Category: ${s.category}
+    </div>
+  `;
 });
